010ab63cdca8710c5b66f81c7c26544c2f008226
singhshivalika <shivalikasiongh14@gmail.com>
2020-05-16 23:08:27 +0530

    UI 60%

--- app/src/main/java/com/singh_shivalika/try_try/SOS.java
+++ app/src/main/java/com/singh_shivalika/try_try/SOS.java
@@ -4,13 +4,14 @@ import android.annotation.SuppressLint;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.provider.ContactsContract;
+import android.telephony.PhoneNumberUtils;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import static android.provider.ContactsContract.CommonDataKinds.Relation.NAME;
 
 public class SOS {
@@ -23,16 +24,33 @@ public class SOS {
         this.context = context;
 
         ContentResolver cr = context.getContentResolver();
-        Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, "DISPLAY_NAME = '" + NAME + "'", null, null);
-        if (cursor.moveToFirst()) {
-            String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-            Cursor phones = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
-            while (phones.moveToNext()) {
-                String number = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                Log.e("LOL",number);
-            }
-            phones.close();
+        String[] proj = new String[]{ ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER};
+        Cursor cursor = cr.query(ContactsContract.Data.CONTENT_URI, proj, null, null, null);
+
+        while(cursor.moveToNext()){
+            try {
+                String s = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                if(PhoneNumberUtils.isEmergencyNumber(s))
+                    Log.e("EMERGENCY",s);
+
+            }catch (Exception e){}
+
         }
+
+
+
+
+        /*Cursor phones = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
+
+        phones.moveToFirst();
+
+        while (phones.moveToNext()) {
+            String number = phones.getString(0);
+            Log.e("LOL",number);
+        }
+        phones.close();*/
+
+
         cursor.close();
     }
 }
